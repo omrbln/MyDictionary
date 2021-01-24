613424629cb1154ab2d55d2e1c73f42ea82d6ea1
ÖMER BİLEN <omer.bilen44@hotmail.com>
2021-01-24 17:05:10 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int> { };
+            dictionary.Add("Ahmet", 16);
+            dictionary.Add("Mehmet", 30);
+
+            for (int i = 0; i < dictionary.keys.Length; i++)
+            {
+                Console.WriteLine(dictionary.keys[i].ToString() + " : " + dictionary.values[i].ToString());
+            }
+
+        }
+    }
+}
